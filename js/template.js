2a03e9a27e4b06bfbb2e248d1d7f0aca0b6a6c21
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-02-27 19:47:36 +0600

    added arrow spread

--- js/template.js
+++ js/template.js
@@ -1,3 +1,23 @@
 const person = 'Adam Sandler';
 const person2 = "Ben White";
 const person3 = `Donald Trum`;
+
+const multiLine = 'First Line text \n' + 
+'second line of code \n' +
+'Fourth line of string \n';
+console.log(multiLine);
+
+const newMultiline = `First line of text
+second line of text
+third line of string 
+fourth line of code`
+// console.log(newMultiline);
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 45, 12, 6, 444]
+const summary = 'sum of: ' + a + ' and ' + b +" is "+ (a+b);
+// console.log(summary);
+
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(newSummary);
